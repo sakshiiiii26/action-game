0a28f1b3669cf4a0ff7157406a9159d0c37dbe9e
Sakshi Sharma <157235573+sakshiiiii26@users.noreply.github.com>
2024-08-11 15:55:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-Document.onkeydown
+Document.onkeydown = function(e){
+   console.log("key code is: ", e.keycode)
+   if(e.keycode==38){
+     doremon = document.querySelector('.doremon')
+     doremon.classList.add('animatedDoremon')
+    }
+}
